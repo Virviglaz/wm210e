15d343e5af3279fdcaba7ce3a0c23ca201504d7e
Pavel Nadein <pavelnadein@gmail.com>
2022-09-07 22:02:35 +0200

    [encoder] update driver
    Signed-off-by: Pavel Nadein <pavelnadein@gmail.com>

--- SW/esp32_firmware/main/main.cpp
+++ SW/esp32_firmware/main/main.cpp
@@ -3,6 +3,9 @@
 #include "menu.h"
 #include "ota.h"
 #include "esp_ota_ops.h"
+#include "esp_encoder.h"
+#include "lcd.h"
+#include <atomic>
 
 extern "C" {
 	void app_main();
@@ -17,6 +20,17 @@ static void ota_confirm(void *args)
 	vTaskDelete(NULL);
 }
 
+static void enc_test()
+{
+	encoder<int> enc(ENC_A, ENC_B);
+	while (1) {
+		delay_ms(300);
+		LCD->clear();
+		LCD->print(FIRST_ROW, CENTER, "%d", enc.get_value());
+		LCD->print(SECOND_ROW, CENTER, "%d", enc.get_noise_level());
+	}
+}
+
 void app_main(void)
 {
 	const esp_app_desc_t *app_desc = esp_ota_get_app_description();
@@ -26,6 +40,8 @@ void app_main(void)
 	int res = hardware_init(app_desc->version);
 	if (res)
 		return;
+
+	//enc_test();
 
 	while (1)
 		menu_start();
